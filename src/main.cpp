ea08c1fafcfd634f6e78965e09ce4c8f545ef1b2
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-02-09 15:28:48 +0300

    complete first task

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void printStdinSum() {
+    int x = 0;
+    int y = 0;
+    cin >> x >> y;
+    cout << (x + y) << "\n";
+}
+
 int main() {
-    cout << "Hello, world\n";
+    printStdinSum();
     return 0;
 }
