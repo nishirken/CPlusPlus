5e6037ef517a84e663aa0585004b7d44482782d2
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-02-10 11:19:32 +0300

    factorial

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,17 @@
 #include <string>
 using namespace std;
 
-void printSmallestString() {
-    string x = "";
-    string y = "";
-    string z = "";
-    cin >> x >> y >> z;
-
-    if (x < y) {
-        if (x < z) {
-            cout << x;
-        } else {
-            cout << z;
-        }
-    } else {
-        if (y < z) {
-            cout << y;
-        } else {
-            cout << z;
-        }
+int Factorial(int x) {
+    if (x <= 1) {
+        return 1;
     }
 
+    return Factorial(x - 1) * x;
+}
+
 int main() {
-    printSmallestString();
+    int x;
+    cin >> x;
+    cout << Factorial(x);
     return 0;
 }
