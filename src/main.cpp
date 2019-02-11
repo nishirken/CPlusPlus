2a59913b4afbc62bbc2e76c1d0693faac420b150
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-02-11 17:32:45 +0300

    UpdateIfGreater

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,16 @@
 #include <vector>
 using namespace std;
 
-void MoveStrings(vector<string>& source, vector<string>& destination) {
-    for (auto s : source) {
-        destination.push_back(s);
+void UpdateIfGreater(int& x, int& y) {
+    if (x > y) {
+        y = x;
     }
-    source.clear();
 }
 
 int main() {
-    vector<string> source = {"a", "b", "c"};
-    vector<string> destination = {"z"};
-    MoveStrings(source, destination);
-    for (auto s : destination) {
-        cout << s << ", ";
-    }
-    cout << endl;
+    int a = 4;
+    int b = 2;
+    UpdateIfGreater(a, b);
+    cout << b;
     return 0;
 }
