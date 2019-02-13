826ae9f045d560f9544d959222467bd0dfed345a
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-02-13 19:25:11 +0300

    start queue

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,88 @@
 #include "print-vector.h"
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
+const int quietHuman = 1;
+const int worryHuman = 0;
+vector<int> queue;
 
-    int sum = 0;
-    string str;
-    vector<int> temperatures(N);
-
-    for (int& t : temperatures) {
-        cin >> t;
-        sum += t;
+void push(int elementsCount) {
+    while(elementsCount--) {
+        queue.push_back(quietHuman);
     }
+}
 
-    int average = sum / N;
-    vector<int> highIndexes;
+void pop(int elementsCount) {
+    while(elementsCount--) {
+        queue.pop_back();
+    }
+}
 
-    for (int i = 0; i <= N; i++) {
-        if (temperatures[i] > average) {
-            highIndexes.push_back(i);
+void markWorry(int index) {
+    for (int i = 0; i <= queue.size(); i++) {
+        if (i == index) {
+            queue[i] = worryHuman;
         }
     }
-    cout << highIndexes.size() << endl;
-    PrintIntVector(highIndexes);
+}
+
+void markQuiet(int index) {
+    for (int i = 0; i <= queue.size(); i++) {
+        if (i == index) {
+            queue[i] = quietHuman;
+        }
+    }
+}
+
+int worryCount() {
+    int temp = 0;
+    for (int human : queue) {
+        if (human == worryHuman) {
+            temp++;
+        }
+    }
+    return temp;
+}
+
+void executeCommand(string command) {
+    int num;
+    string temp;
+
+    if (command == "WORRY_COUNT") {
+        cout << worryCount() << endl;
+    } else {
+        temp = command[command.length() - 1];
+        num = (int)temp[0] - 48;
+        if (command == "COME") {
+            if (num > 0) {
+                push(num);
+            } else {
+                pop(-num);
+            }
+        } else if (command == "WORRY") {
+            markWorry(num);
+        } else if (command == "QUIET") {
+            markQuiet(num);
+        }
+    }
+}
+
+int main() {
+    int Q;
+    cin >> Q;
+    vector<string> commands(Q);
+
+    if (Q == 0) {
+        return 0;
+    }
+
+    for (int i = 1; i < Q; i++) {
+        getline(cin, commands[i]);
+    }
+    PrintStringVector(commands);
+
+//    for (auto command : commands) {
+//        executeCommand(command);
+//    }
 
     return 0;
 }
--- src/print-vector.h
+++ src/print-vector.h
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 void PrintIntVector(const std::vector<int>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i];
+        if (i != v.size() - 1) {
+            std::cout << ' ';
+        }
+    }
+    std::cout << std::endl;
+}
+
+void PrintStringVector(const std::vector<string>& v) {
     for (int i = 0; i < v.size(); i++) {
         std::cout << v[i];
         if (i != v.size() - 1) {
