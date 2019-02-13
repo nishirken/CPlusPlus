29be76173ac5d792cf7bfc5cc4b11e0344ce7096
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-02-13 11:11:54 +0300

    complete temperature

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,32 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
-#include <stdlib.h>
 #include "print-vector.h"
 using namespace std;
 
 int main() {
     int N;
     cin >> N;
-    double sum = 0;
-    vector<int> temperatures(N);
-    string temp;
-    getline(cin, temp);
 
-    for (int i = 0; i <= N; i++) {
-        temperatures[i] = (int)temp[0] - 48;
-        sum += temperatures[i];
+    int sum = 0;
+    string str;
+    vector<int> temperatures(N);
+
+    for (int& t : temperatures) {
+        cin >> t;
+        sum += t;
     }
 
-    double average = sum / N;
-    int counter = 0;
+    int average = sum / N;
     vector<int> highIndexes;
 
     for (int i = 0; i <= N; i++) {
         if (temperatures[i] > average) {
-            counter++;
             highIndexes.push_back(i);
         }
     }
-//    cout << counter << endl;
-//    PrintIntVector(highIndexes);
+    cout << highIndexes.size() << endl;
+    PrintIntVector(highIndexes);
 
     return 0;
 }
