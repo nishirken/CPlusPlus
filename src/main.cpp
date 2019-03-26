cdf830987cd3d0b9780fb5f4e1e9cb17146090e5
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-03-26 21:51:49 +0300

    add map countries

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,53 @@
 // #include "print-vector.h"
 using namespace std;
 
-string isAnagram(string first, string second) {
-    sort(first.begin(), first.end());
-    sort(second.begin(), second.end());
-    if (first == second) {
-        return "YES";
+map<string, string> capitals;
+
+string changeCapital(string country, string newCapital) {
+    string res;
+    if (capitals.count(country)) {
+        if (capitals[country] == newCapital) {
+            res = "Country " + country + " hasn't changed its capital";
+        } else {
+            res = "Country " + country +
+                " has changed its capital from " + capitals[country] + " to " + newCapital;
+        }
     } else {
-        return "NO";
+        res = "Introduce new country " + country + " with capital " + newCapital;
+    }
+    capitals[country] = newCapital;
+    return res;
+}
+
+string rename(string oldCountryName, string newCountryName) {
+    if (oldCountryName == newCountryName || !capitals.count(oldCountryName) || capitals.count(newCountryName)) {
+        return "Incorrect rename, skip";
+    } else {
+        capitals[newCountryName] = capitals[oldCountryName];
+        capitals.erase(oldCountryName);
+        return "Country " + oldCountryName +
+            " with capital " + capitals[newCountryName] + " has been renamed to " + newCountryName;
+    }
+}
+
+string about(string country) {
+    if (capitals.count(country)) {
+        return "Country " + country + " has capital " + capitals[country];
+    } else {
+        return "Country " + country + " doesn't exist";
+    }
+}
+
+string dump() {
+    if (capitals.empty()) {
+        return "There are no countries in the world";
+    } else {
+        string temp;
+        for (auto const& x : capitals) {
+            temp += (x.first + "/" + x.second + " ");
+        }
+        temp.pop_back();
+        return temp;
     }
 }
 
@@ -52,9 +92,21 @@ string getCommand(string inputStr) {
 }
 
 void executeCommand(string command) {
-    string first = dropLast(command);
-    string second = getLast(command);
-    cout << isAnagram(first, second) << endl;
+    string cmd = getCommand(command);
+    if (cmd == "CHANGE_CAPITAL") {
+        string capital = getLast(command);
+        string country = getLast(dropLast(command));
+        cout << changeCapital(country, capital) << endl;
+    } else if (cmd == "RENAME") {
+        string newCountryName = getLast(command);
+        string oldCountryName = getLast(dropLast(command));
+        cout << rename(oldCountryName, newCountryName) << endl;
+    } else if (cmd == "ABOUT") {
+        string country = getLast(command);
+        cout << about(country) << endl;
+    } else if (cmd == "DUMP") {
+        cout << dump() << endl;
+    }
 }
 
 int main() {
