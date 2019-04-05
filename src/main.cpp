94a9c3421d74c1fbde637467c8f83b90407f721c
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-04-05 19:10:09 +0300

    white-4: add Specialization struct

--- src/main.cpp
+++ src/main.cpp
@@ -7,53 +7,44 @@
 // #include "print-vector.h"
 using namespace std;
 
-vector<string> split(const string& str, const char& delim) {
-    vector<string> acc;
-    string temp;
-    for (const char& c : str) {
-        if (c == delim) {
-            acc.push_back(temp);
-            temp = "";
-            continue;
-        }
-        temp.push_back(c);
-    }
-    acc.push_back(temp);
-    return acc;
-}
+struct Specialization {
+  string value;
+  explicit Specialization(const string& newValue) {
+    value = newValue;
+  }
+};
 
-map<set<string>, string> stopsBusMap;
+struct Course {
+  string value;
+  explicit Course(const string& newValue) {
+    value = newValue;
+  }
+};
 
-void executeCommand(string command) {
-    vector<string> input = split(command, ' ');
-    string busNumber = input[0];
-    set<string> stops = set<string>(input.begin() + 1, input.end());
+struct Week {
+  string value;
+  explicit Week(const string& newValue) {
+    value = newValue;
+  }
+};
 
-    if (!stopsBusMap.count(stops)) {
-        stopsBusMap[stops] = to_string(stopsBusMap.size() + 1);
-        cout << "New bus " + to_string(stopsBusMap.size()) << endl; 
-    } else {
-        cout << "Already exists for " + stopsBusMap[stops] << endl;
-    }
-}
+struct LectureTitle {
+  string specialization;
+  string course;
+  string week;
+
+  explicit LectureTitle(const Specialization& spec, const Course& c, const Week& w) {
+    specialization = spec.value;
+    course = c.value;
+    week = w.value;
+  }
+};
 
 int main() {
-    int Q;
-    cin >> Q;
-    vector<string> commands(Q);
-    string s;
-    getline(cin, s);
-    if (Q == 0) {
-        return 0;
-    }
-
-    for (string& c : commands) {
-        getline(cin, c);
-    }
-
-    for (auto command : commands) {
-        executeCommand(command);
-    }
-
-    return 0;
+  LectureTitle title(
+    Specialization("C++"),
+    Course("White belt"),
+    Week("4th")
+  );
+  return 0;
 }
