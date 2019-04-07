1a5e7f6a58a93bf4ed8229d5b76ac2e5659d1b81
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-04-07 18:37:16 +0300

    white-4: table output

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,47 @@
 // #include "print-vector.h"
 using namespace std;
 
+vector<string> split(const string& str, const char& delim) {
+    vector<string> acc;
+    string temp;
+    for (const char& c : str) {
+        if (c == delim) {
+            acc.push_back(temp);
+            temp = "";
+            continue;
+        }
+        temp.push_back(c);
+    }
+    acc.push_back(temp);
+    return acc;
+}
+
 int main() {
   ifstream input("input.txt");
 
   string line;
   if (input) {
-    cout << fixed << setprecision(3);
-    while (getline(input, line)) {
-      cout << stod(line) << endl;
+    getline(input, line);
+    vector<string> splitted(split(line, ' '));
+    vector<vector<string>> data;
+    
+    while(getline(input, line)) {
+      vector<string> stringData = split(line, ',');
+      vector<string> l;
+      for (auto const& s : stringData) {
+        l.push_back(s);
+      }
+      data.push_back(l);
+    }
+
+    for (auto const& d : data) {
+      for (int i = 0; i != d.size(); i++) {
+        cout << setw(10) << d[i];
+        if (i < d.size() - 1) {
+          cout << " ";
+        }
+      }
+      cout << endl;
     }
   }
   return 0;
